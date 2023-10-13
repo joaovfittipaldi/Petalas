91e1ea20f3aa414f6a0bd7b71bbb9bda87ac60f2
iaa877 <iaa@cesar.school>
2023-10-13 20:39:04 -0300

    navbar

--- petala/petalas_app/urls.py
+++ petala/petalas_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('criancas/', views.listagem_crianca, name='listagem_criancas'),
     path('home/', views.home, name='home'),
+    path('navbar/', views.navbar, name='navbar'),
 ]
--- petala/petalas_app/views.py
+++ petala/petalas_app/views.py
@@ -29,3 +29,6 @@ def listagem_crianca(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def navbar(request):
+    return render(request, 'navbar.html')
